e1ba1d6ae41f9e3e52c1f5542010ee5bc9cced89
Scott Crawford <scottcrawford@Scotts-MacBook-Air-2.local>
2016-07-08 15:02:48 -0500

    Update Array Search

--- search-arrays.php
+++ search-arrays.php
@@ -5,19 +5,39 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 
-$result = array_search('Tina', $names, true);
-var_dump($result);
+// $result = array_search('Tina', $names, true);
+// var_dump($result);
 
-$result = array_search('Tina', $names, false);
-var_dump($result);
+// $result = array_search('Tina', $names, false);
+// var_dump($result);
 
+// $result = array_search('Bob', $names, true);
+// var_dump($result);
 
-$result = array_search('Bob', $names, true);
-var_dump($result);
+// $result = array_search('Bob', $names, false);
+// var_dump($result);
 
-$result = array_search('Bob', $names, false);
-var_dump($result);
+// $intersect = array_intersect($names , $compare);
+// var_dump($intersect);
 
-$intersect = array_intersect($names , $compare);
-var_dump($intersect);
+$find = 'Amy';
+
+function inArray ($find, $names) {
+	if (array_search($find, $names) === false) {
+		echo "False\n";
+	} else {
+		echo "True\n";
+	}
+}
+inArray($find, $names);
+
+$i=0;
+$count = 0;
+foreach($names as $name){
+	if(array_search($name, $compare) !== false) {
+		$count++;
+	}
+
+};
+echo "$count\n";
 
